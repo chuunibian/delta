5a2db4fce2fe5927b5448e88a3a7cc2d6bd123af
Austin Bian <iaustin0b@gmail.com>
2026-01-27 00:33:47 -0500

    Add copy path and reveal in FS explorer for path and add required plugins

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -2,6 +2,8 @@ import { clsx, type ClassValue } from "clsx"
 import { filesize } from "filesize";
 import { twMerge } from "tailwind-merge"
 
+const windowsPathSeparator = '\\';
+
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
@@ -9,4 +11,17 @@ export function cn(...inputs: ClassValue[]) {
 export const formatBytes = (bytes: any) => {
   if (!bytes) return "0 B";
   return filesize(Number(bytes), { base: 2, standard: "jedec" }) as string;
+}
+
+// Only really for on windows the root dir since root dir has the / appended to it
+export function appendPaths (prevPath: string, currentPath: string) {
+  if (!prevPath) {
+    return currentPath;
+  }
+
+  if (prevPath.endsWith(windowsPathSeparator)) {
+    return `${prevPath}${currentPath}`
+  }
+
+  return `${prevPath}\\${currentPath}`
 }
