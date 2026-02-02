b3f99cf8528c874d4ca4685dd32663744f077d46
Austin Bian <iaustin0b@gmail.com>
2026-02-01 19:09:03 -0500

    Delete unused react components

--- src-tauri/src/preload.rs
+++
@@ -1,8 +0,0 @@
-use core::{error, panic};
-use std::{collections::HashMap, fs, hash::Hash, path::PathBuf, thread::current, time::SystemTime};
-
-use walkdir::WalkDir;
-
-use crate::model;
-
-// pub fn load_settings() {}
