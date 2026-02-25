a434c97a4492d29acaa886d84cb2595562d79b3b
Austin Bian <iaustin0b@gmail.com>
2026-02-25 15:16:47 -0500

    Fix for borrow error

--- src-tauri/src/platform/linux.rs
+++ src-tauri/src/platform/linux.rs
@@ -17,12 +17,8 @@ pub fn appdata_folder_check(local_appdata_path: &Path) -> Result<(), AppError> {
     for entry_result in entries {
         let entry = entry_result?;
         let entry_path = entry.path();
-        let file_name_parts: Vec<&str> = entry
-            .file_name()
-            .to_string_lossy()
-            .to_string()
-            .split('_')
-            .collect();
+        let entry_path_string = entry.file_name().to_string_lossy().to_string();
+        let file_name_parts: Vec<&str> = entry_path_string.split('_').collect();
 
         if file_name_parts.len() != 3 {
             fs::remove_file(entry_path)?;
