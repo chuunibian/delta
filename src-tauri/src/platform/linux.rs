d7937277ceeb64e485a0c0f1f7874e96cf6a4ff6
Austin Bian <iaustin0b@gmail.com>
2026-02-25 14:48:44 -0500

    Add preload checks to local snaphsot storage folder file names

--- src-tauri/src/platform/linux.rs
+++ src-tauri/src/platform/linux.rs
@@ -4,6 +4,42 @@ use std::path::Path;
 use crate::error::AppError;
 
 pub fn appdata_folder_check(local_appdata_path: &Path) -> Result<(), AppError> {
+    let snapshot_folder = local_appdata_path.join("tempsnapshot");
+
+    if !snapshot_folder.exists() {
+        return Err(AppError::StartupError(
+            "Local storage snapshot folder does not exist.".to_string(),
+        ));
+    }
+
+    let entries = fs::read_dir(snapshot_folder)?;
+
+    for entry_result in entries {
+        let entry = entry_result?;
+        let entry_path = entry.path();
+        let file_name_parts: Vec<&str> = entry
+            .file_name()
+            .to_string_lossy()
+            .to_string()
+            .split('_')
+            .collect();
+
+        if file_name_parts.len() != 3 {
+            fs::remove_file(entry_path)?;
+            return (AppError::StartupError(
+                "A file in the local storage failed to satisfy name constraints".to_string(),
+            ));
+        }
+
+        if file_name_parts[1].len() != 12 {
+            fs::remove_file(entry_path)?;
+            return Err(AppError::StartupError(
+                "A file in the local storage failed to satisfy name constraints. Date is wrong"
+                    .to_string(),
+            ));
+        }
+    }
+
     Ok(())
 }
 
--- src-tauri/src/platform/windows.rs
+++ src-tauri/src/platform/windows.rs
@@ -6,37 +6,41 @@ use crate::error::AppError;
 pub fn appdata_folder_check(local_appdata_path: &Path) -> Result<(), AppError> {
     let snapshot_folder = local_appdata_path.join("tempsnapshot");
 
-    if snapshot_folder.exists() {
-        let entries = fs::read_dir(snapshot_folder)?;
+    if !snapshot_folder.exists() {
+        return Err(AppError::StartupError(
+            "Local storage snapshot folder does not exist.".to_string(),
+        ));
+    }
 
-        for entry_result in entries {
-            let entry = entry_result?;
-            let entry_path = entry.path();
-            let entry_path_string = entry.file_name().to_string_lossy().to_string();
-            let file_name_parts: Vec<&str> = entry_path_string.split('_').collect();
+    let entries = fs::read_dir(snapshot_folder)?;
 
-            if file_name_parts.len() != 3 {
-                fs::remove_file(entry_path)?;
-                return Err(AppError::StartupError(
-                    "A file in the local storage failed to satisfy name constraints".to_string(),
-                ));
-            }
+    for entry_result in entries {
+        let entry = entry_result?;
+        let entry_path = entry.path();
+        let entry_path_string = entry.file_name().to_string_lossy().to_string();
+        let file_name_parts: Vec<&str> = entry_path_string.split('_').collect();
 
-            if file_name_parts[0].len() != 1 {
-                fs::remove_file(entry_path)?;
-                return Err(AppError::StartupError(
-                    "A file in the local storage failed to satisfy name constraints. Drive letter is wrong".to_string(),
-                ));
-            }
+        if file_name_parts.len() != 3 {
+            fs::remove_file(entry_path)?;
+            return Err(AppError::StartupError(
+                "A file in the local storage failed to satisfy name constraints".to_string(),
+            ));
+        }
 
-            // date is big endian and always 12
-            if file_name_parts[1].len() != 12 {
-                fs::remove_file(entry_path)?;
-                return Err(AppError::StartupError(
-                    "A file in the local storage failed to satisfy name constraints. Date is wrong"
-                        .to_string(),
-                ));
-            }
+        if file_name_parts[0].len() != 1 {
+            fs::remove_file(entry_path)?;
+            return Err(AppError::StartupError(
+                "A file in the local storage failed to satisfy name constraints. Drive letter is wrong".to_string(),
+            ));
+        }
+
+        // date is big endian and always 12
+        if file_name_parts[1].len() != 12 {
+            fs::remove_file(entry_path)?;
+            return Err(AppError::StartupError(
+                "A file in the local storage failed to satisfy name constraints. Date is wrong"
+                    .to_string(),
+            ));
         }
     }
 
